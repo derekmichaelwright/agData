5937334310fe19d6069cb5ecc61da1179acf200c
derekmichaelwright <derek.wright@usask.ca>
2019-01-19 13:10:20 -0600

    edit

--- R/addRegionInfo.R
+++ R/addRegionInfo.R
@@ -8,7 +8,8 @@
 #' xx <- agData_FAO_Crops %>% addRegionInfo()
 
 addRegionInfo <- function(x) {
-  regions <- dplyr::bind_rows(agData_FAO_Country_Table, agData_FAO_Region_Table)
-  x <- dplyr::left_join(x, select(regions, Area = Country, Region, SubRegion, DVDDVG, Lat, Lon), by = "Area")
+  regions <- bind_rows(agData_FAO_Country_Table, agData_FAO_Region_Table) %>%
+    filter(!is.na(Country)) %>% filter(!duplicated(Country))
+  x <- left_join(x, select(regions, Area = Country, Region, SubRegion, DVDDVG, Lat, Lon), by = "Area")
   x
 }
