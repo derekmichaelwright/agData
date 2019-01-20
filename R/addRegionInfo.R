168dd144f0cb0ac1b58bb4a9ff67a635cbc74f86
derekmichaelwright <derek.wright@usask.ca>
2019-01-20 10:36:26 -0600

    edit

--- R/addRegionInfo.R
+++ R/addRegionInfo.R
@@ -10,6 +10,6 @@
 addRegionInfo <- function(x) {
   regions <- bind_rows(agData_FAO_Country_Table, agData_FAO_Region_Table) %>%
     filter(!is.na(Country)) %>% filter(!duplicated(Country))
-  x <- left_join(x, select(regions, Area = Country, Region, SubRegion, DVDDVG, Lat, Lon), by = "Area")
+    x <- suppressWarnings(left_join(x, select(regions, Area = Country, Region, SubRegion, DVDDVG, Lat, Lon), by = "Area") )
   x
 }
