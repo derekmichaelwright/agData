8c99faa1f14b63c01cfafecae5616106a7d296b7
derekmichaelwright <derek.wright@usask.ca>
2019-09-03 07:22:00 -0600

    edit

--- R/theme_agData.R
+++ R/theme_agData.R
@@ -8,5 +8,6 @@
 #' xx <- agData_FAO_Crops %>% agData_spread()
 
 theme_agData <- function(x) {
-  theme_bw() + theme(strip.background = element_rect(fill = "White"))
+  theme(panel.background = element_rect(colour = "black"),
+        strip.background = element_rect(colour = "black"))
 }
