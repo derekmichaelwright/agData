82a762f4ad5409c6ec333147e078567857bf529c
derekmichaelwright <derek.wright@usask.ca>
2020-01-12 08:59:28 -0600

    edit

--- R/theme_agData.R
+++ R/theme_agData.R
@@ -9,18 +9,19 @@
 #' @return ggplot
 #' @export
 
-theme_agData <- function(x, linesize = 0.75, bgFill = "grey95", stripFill = "white", lineColor = "white", rotx = F) {
+theme_agData <- function(x, linesize = 0.75, bgFill = "grey95", stripFill = "white", lineColor = "white", rotx = F, ...) {
   if(rotx == T) {
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
           strip.background = element_rect(colour = "black", fill = stripFill, size = linesize),
           panel.grid = element_line(color = lineColor),
           panel.background = element_rect(colour = "black", fill = bgFill, size = linesize),
-          panel.border = element_rect(colour = "black", fill=NA, size=linesize))
+          panel.border = element_rect(colour = "black", fill=NA, size=linesize),
+          ...)
   }else{
     theme(strip.background = element_rect(colour = "black", fill = stripFill, size = linesize),
           panel.grid = element_line(color = lineColor),
           panel.background = element_rect(colour = "black", fill = bgFill, size = linesize),
-          panel.border = element_rect(colour = "black", fill=NA, size=linesize))
+          panel.border = element_rect(colour = "black", fill=NA, size=linesize),
+          ...)
   }
-  
 }
