fa36857e86feeaad1fcc3db99d8a92675b48fad8
derekmichaelwright <derek.wright@usask.ca>
2020-01-15 22:43:43 -0600

    edit

--- R/theme_agData.R
+++ R/theme_agData.R
@@ -11,7 +11,7 @@
 
 theme_agData <- function(x, linesize = 0.75, bgFill = "grey95", stripFill = "white", lineColor = "white", rotx = F, ...) {
   if(rotx == T) {
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5),
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
           strip.background = element_rect(colour = "black", fill = stripFill, size = linesize),
           panel.grid = element_line(color = lineColor),
           panel.background = element_rect(colour = "black", fill = bgFill, size = linesize),
