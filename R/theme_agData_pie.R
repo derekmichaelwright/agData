77a38efd1b37e7293576d6e85d1dcee783868930
derekmichaelwright <derek.wright@usask.ca>
2022-03-31 15:46:42 -0500

    edit

--- R/theme_agData_pie.R
+++ R/theme_agData_pie.R
@@ -9,6 +9,7 @@
 theme_agData_pie <- function(x, ...) {
   theme(panel.background = element_blank(),
         panel.border = element_blank(),
+        axis.title = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
         ...)
