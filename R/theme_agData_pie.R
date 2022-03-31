5f1d822ea2b642497142f47c5c61c5b564a4e952
derekmichaelwright <derek.wright@usask.ca>
2022-03-31 15:38:10 -0500

    edit

---
+++ R/theme_agData_pie.R
@@ -0,0 +1,15 @@
+#' theme_agData_pie.
+#'
+#' ggplot theme.
+#' @param x ggplot
+#' @return ggplot with agData theme for pie graphs
+#' @export
+#' @examples
+
+theme_agData_pie <- function(x, ...) {
+  theme(panel.background = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        ...)
+}
